c578cb815218a0b12d710461679907a239af58c3
Xancake <xanylein@web.de>
2015-12-15 16:14:47 +0100

    Dateien für Praktikum 3 angelegt:
    - Example (wie in Aufgabe beschrieben aus Kapitel 4.1.2 des
    Tutorial-Handbuchs zu Choco)
    - SpaltenweiseMitUebertraegen (Kopie von Example, muss noch angepasst
    werden, sodass spaltenweise mit Übertrag verarbeitet wird)

---
+++ BAI4_CI_Praktikum/src/org/haw/cip/praktikum3/choco/ConstraintSolverSpaltenweiseMitUebertraegen.java
@@ -0,0 +1,65 @@
+package org.haw.cip.praktikum3.choco;
+
+import choco.Choco;
+import choco.Options;
+import choco.cp.model.CPModel;
+import choco.cp.solver.CPSolver;
+import choco.kernel.model.Model;
+import choco.kernel.model.variables.integer.IntegerExpressionVariable;
+import choco.kernel.model.variables.integer.IntegerVariable;
+import choco.kernel.solver.Solver;
+
+public class ConstraintSolverSpaltenweiseMitUebertraegen {
+	public static void main(String[] arg) {
+		Model model = new CPModel();
+		
+		// Declare every letter as a variable
+		IntegerVariable d = Choco.makeIntVar("d", 0, 9, new String[]{Options.V_ENUM});
+		IntegerVariable o = Choco.makeIntVar("o", 0, 9, new String[]{Options.V_ENUM});
+		IntegerVariable n = Choco.makeIntVar("n", 0, 9, new String[]{Options.V_ENUM});
+		IntegerVariable a = Choco.makeIntVar("a", 0, 9, new String[]{Options.V_ENUM});
+		IntegerVariable l = Choco.makeIntVar("l", 0, 9, new String[]{Options.V_ENUM});
+		IntegerVariable g = Choco.makeIntVar("g", 0, 9, new String[]{Options.V_ENUM});
+		IntegerVariable e = Choco.makeIntVar("e", 0, 9, new String[]{Options.V_ENUM});
+		IntegerVariable r = Choco.makeIntVar("r", 0, 9, new String[]{Options.V_ENUM});
+		IntegerVariable b = Choco.makeIntVar("b", 0, 9, new String[]{Options.V_ENUM});
+		IntegerVariable t = Choco.makeIntVar("t", 0, 9, new String[]{Options.V_ENUM});
+		
+		// Declare every name as a variable
+		IntegerVariable donald = Choco.makeIntVar("donald", 0, 1000000,  new String[]{Options.V_BOUND});
+		IntegerVariable gerald = Choco.makeIntVar("gerald", 0, 1000000,  new String[]{Options.V_BOUND});
+		IntegerVariable robert = Choco.makeIntVar("robert", 0, 1000000,  new String[]{Options.V_BOUND});
+		
+		// Array of coefficients
+		int[] c = new int[]{100000, 10000, 1000, 100, 10, 1};
+		
+		// Declare every combination of letter as an integer expression
+		IntegerExpressionVariable donaldLetters = Choco.scalar(new IntegerVariable[]{d, o, n, a, l, d}, c);
+		IntegerExpressionVariable geraldLetters = Choco.scalar(new IntegerVariable[]{g, e, r, a, l, d}, c);
+		IntegerExpressionVariable robertLetters = Choco.scalar(new IntegerVariable[]{r, o, b, e, r, t}, c);
+		
+		// Add equality between name and letters combination
+		model.addConstraint(Choco.eq(donaldLetters, donald));
+		model.addConstraint(Choco.eq(geraldLetters, gerald));
+		model.addConstraint(Choco.eq(robertLetters, robert));
+		
+		// Add constraint name sum
+		model.addConstraint(Choco.eq(Choco.plus(donald, gerald), robert));
+		
+		// Add constraint of all different letters.
+		model.addConstraint(Choco.allDifferent(new IntegerVariable[]{d, o, n, a, l, g, e, r, b, t}));
+		
+		// Build a solver, read the model and solve it
+		Solver s = new CPSolver();
+		s.read(model);
+		
+		
+		long start = System.currentTimeMillis();
+		s.solve();
+		System.out.println("Laufzeit: " + (System.currentTimeMillis() - start) + "ms");
+		System.out.println("############################");
+		System.out.println("donald = " + s.getVar(donald).getVal());
+		System.out.println("gerald = " + s.getVar(gerald).getVal());
+		System.out.println("robert = " + s.getVar(robert).getVal());
+	}
+}
